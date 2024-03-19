33f57181e2933482f51313c73bd4df23aded0af3
samifr <sfauzanramadhan@gmail.com>
2024-03-19 08:58:18 +0000

    [Add] document required packages

---
+++ install_external_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install -y ros-noetic-robot-localization \
+    ros-noetic-slam-gmapping \
+    ros-noetic-hector-slam \
+    ros-noetic-navigation \
+    ros-noetic-rosserial-arduino \
+    ros-noetic-map-server
